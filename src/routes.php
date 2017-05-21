a0703fa10f2faac0ad0f0c958bab8174a7baa3a2
Anna Dabrowska <dabrowska@cosmocode.de>
2017-05-21 20:29:08 +0200

    Define sample routes

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,24 @@
 <?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', 'HomeController:index')->setName('home');
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/home', function (Request $request, Response $response) {
+    return $this->view->render($response, 'home.twig');
 });
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->get('/login', 'AuthenticationController:login')->setName('login');
+$app->post('/login', 'AuthenticationController:in');
+
+$app->get('/logout', 'AuthenticationController:logout')->setName('logout');
+$app->post('/logout', 'AuthenticationController:out');
